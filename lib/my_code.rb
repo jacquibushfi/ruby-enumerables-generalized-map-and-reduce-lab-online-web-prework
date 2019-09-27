da2d35c304cab8a72c53ca933e1a533dee6ddda2
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-09-27 19:41:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,12 +1,11 @@
-# Your Code Here
+
 def map(array)
   new = []
   i = 0
   while i < array.length
-  yield(array[i])
-    new.push(array[i] * -1 ) 
+    new.push(yield(array[i] * -1 )) 
     i += 1
   end
-  return new
+ new
 end
   
